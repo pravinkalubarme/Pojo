f1c4f074df8d06dd84db8c5b0846f7829205748f
pravinkalubarme <55593158+pravinkalubarme@users.noreply.github.com>
2019-12-16 15:10:19 +0530

    Update TestClass.java

--- src/test/java/Inn/Pojo/TestClass.java
+++ src/test/java/Inn/Pojo/TestClass.java
@@ -11,21 +11,21 @@ public class TestClass {
 	@Test
 	public void Example2()
 	{
-		RestAssured.baseURI="https://reqres.in";
-		Pojo response = given().expect().defaultParser(Parser.JSON).when().get("/api/unknown").as(Pojo.class);
+	String[] expectedNames= {"cerulean","fuchsia rose","true red","aqua sky","tigerlily","blue turquoise"};
+	List<String> expectedListOfNames = Arrays.asList(expectedNames);
+	RestAssured.baseURI="https://reqres.in";
+	Pojo response = given().expect().defaultParser(Parser.JSON).when().get("/api/unknown").as(Pojo.class);
 
         Assert.assertEquals(1, response.getPage());
         Assert.assertEquals(6, response.getPer_page());
         Assert.assertEquals(12, response.getTotal());
         Assert.assertEquals(2, response.getTotal_pages());
+        
+        ArrayList<String> actualListOfNames= new ArrayList<String>();
         for (int i = 0; i < response.getData().size(); i++) {
-        	 DataForPojo contentsOfData = response.getData().get(i);
-        	System.out.println(contentsOfData.getId());
-          	System.out.println(contentsOfData.getName());
-          	System.out.println(contentsOfData.getColor());
-          	System.out.println(contentsOfData.getYear());
-          	System.out.println(contentsOfData.getPantone_value());
-          	System.out.println();
+           String colorName = response.getData().get(i).getName();
+           actualListOfNames.add(colorName);
 		}
+        Assert.assertEquals(actualListOfNames, expectedListOfNames);
 	}
 }
